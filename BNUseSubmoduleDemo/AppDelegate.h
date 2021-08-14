46c0f78d0f75c807b4646cb62f55614260faec92
binbinwang <307649087@qq.com>
2021-08-15 01:33:11 +0800

    Initial Commit

---
+++ BNUseSubmoduleDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  BNUseSubmoduleDemo
+//
+//  Created by binbinwang on 2021/8/15.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ BNUseSubmoduleDemo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  BNUseSubmoduleDemo
+//
+//  Created by binbinwang on 2021/8/15.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
